e0b01021980da94b5637aab6209020358e8fd9af
TTL-cell <tjdmstjtjdms@gmail.com>
2026-05-12 15:11:57 +0900

    Update dataset_llama.sh

--- scripts/tools/process_data/dataset_llama.sh
+++ scripts/tools/process_data/dataset_llama.sh
@@ -1,12 +1,9 @@
 BASE_PATH=$1
 
-# if tokenize_vox_llama.py로 했다면 -> generate_metadata_only.py로 wav_path.jsonl 만들기
-#  tokenize_vox_llama_metajsonl.py -> wav_path.jsonl 저절로 생성됨
-
 export PYTHONPATH=${BASE_PATH}
 python3 tools/process_data/tokenize_llama_metajsonl.py \
     --base-path $BASE_PATH \
-    --model-path /data/esseo/DB/TTS/tokenizer/smoll2-135m-ref/tokenizer \
+    --model-path  \
     --data-dir data/voxbox/train \
     --save processed_data_no_chunk/pretrain_train \
     --data-name voxbox \
@@ -15,5 +12,5 @@ python3 tools/process_data/tokenize_llama_metajsonl.py \
     --data-process-workers 32 \
     --model-type llama \
     --chunk-num-per-shard 1000000 \
-    --audio-token-dir /data/esseo/PycharmProjects/SparkVox/local/bicodec \
+    --audio-token-dir  \
     --gt-num 32
